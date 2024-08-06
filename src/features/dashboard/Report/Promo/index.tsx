25eb98df08f5839d6937ed383a5e48cb8c4bf5ef
CaturAgeng <caturageng023@gmail.com>
2024-08-06 13:37:01 +0700

    Writing a code for report promo page

--- src/features/dashboard/Report/Promo/index.tsx
+++ src/features/dashboard/Report/Promo/index.tsx
@@ -1,8 +1,22 @@
 export default function PromoPage() {
     return (
-        <div>
-            <h1>Promo Report Page</h1>
-            <p>Details about reports...</p>
+        <div className="w-full flex flex-col items-center">
+            <div className="w-full">
+                <div className="flex justify-between items-center pt-20 mb-4 gap-6">
+                    <div className="text-gray-2">
+                        <h2 className="text-xl font-bold">Report</h2>
+                        <p className="text-sm">Check your current store report</p>
+                    </div>
+                </div>
+
+                <div className="p-4 bg-white border border-gray-6 rounded-[10px] shadow-md mb-4">
+                    <h1 className="text-2xl ">Filter Data Report</h1>
+                    <p className="text-sm text-gray-2">Filter your summary for detail data report</p>
+                </div>
+                <div className="p-4 bg-white border border-gray-6 rounded-[10px] shadow-md">
+                    <h1 className="text-2xl ">Promo Summary</h1>
+                </div>
+            </div>
         </div>
     );
 }
