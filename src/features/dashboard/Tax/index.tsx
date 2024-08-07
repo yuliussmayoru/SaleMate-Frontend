53701fa3eb680bb9f009a5a08aeb09fe8fc9b92d
CaturAgeng <caturageng023@gmail.com>
2024-08-07 21:28:22 +0700

    Updated code

--- src/features/dashboard/Tax/index.tsx
+++ src/features/dashboard/Tax/index.tsx
@@ -144,8 +144,8 @@ export default function TaxPage() {
                             >
                                 <path d="M8.25 4.5L15.75 12L8.25 19.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
-                            </div>
                         </div>
+                    </div>
                         <div>
                             <table className="min-w-full table-fixed text-center">
                                 <thead className=" text-gray-4">
