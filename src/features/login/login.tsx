3d2eda15026a9b15c3cbb87fdfb25d594e147bdb
jodyboy <elite.crew66@gmail.com>
2024-08-17 11:12:18 +0700

    feat/login - add image, logo, and text in login and pin page

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "@/src/features";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
@@ -88,7 +89,7 @@ export function LoginForm() {
                                 {showPassword ? 'Hide' : 'Show'}
                             </button>
                         </div>
-                        <div className="flex justify-between w-full mt-4 gap-2">
+                        <div className="flex justify-between w-full gap-2">
                             <Button
                                 type="submit"
                                 className="text-xl"
@@ -96,15 +97,57 @@ export function LoginForm() {
                                 Log In
                             </Button>
                         </div>
-                        <p>Forget password</p>
+                        {/* <p>Forget password</p> */}
                     </form>
-                    <div className="flex flex-col justify-center items-center">
-                        <span>have POS pin?</span>
+                    <div className="flex flex-col justify-center items-center mb-2">
+                        <span className="mb-2">have POS pin?</span>
                         <Link href="/pin" className="text-lg w-full">
                             <Button className="bg-green-2 hover:bg-green-1">
                                 Log In POS
                             </Button>
                         </Link>
+                    </div>
+
+                    <div className="absolute flex flex-row top-0 left-0 -z-50 w-full h-full justify-between p-10">
+                        <div>
+                            <Image
+                                height="903"
+                                width="500"                            
+                                src="/login-img.png"
+                                alt="Salemate Logo"
+                            />
+                        </div>
+                        <div>
+                            <Image
+                                height="100"
+                                width="100"                            
+                                src="/favicon.png"
+                                alt="Salemate Logo"
+                            />
+                        </div>
+                        <div className="text-[3rem] text-white font-bold w-[500px]">
+                            <div className="h-full flex flex-col justify-center text-left">
+                                <h1 className="-mt-3">
+                                    working is like
+                                </h1>
+                                <h1 className="-mt-3">
+                                    doing a hobby,
+                                </h1>
+                                <br />
+                                <h1 className="-mt-3">
+                                    that's how it
+                                </h1>
+                                <h1 className="-mt-3">
+                                    feels when using
+                                </h1>
+                                <h1 className="-mt-3">
+                                    Salemate
+                                </h1>
+                            </div>
+                            <div className="absolute text-[20rem] -mt-[51rem] -ml-[3rem] opacity-30">
+                                <h1>“</h1>
+                            </div>
+                        </div>
                     </div>
                 </Card>
             </div>
