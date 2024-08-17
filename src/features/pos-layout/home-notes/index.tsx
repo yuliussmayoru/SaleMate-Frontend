b5af9d7416307eaff4195f017a65b41d3dd94a5f
yuliussmayoru <yuliussmayoru@gmail.com>
2024-08-17 12:40:13 +0700

    feat: add pos-home in pages

---
+++ src/features/pos-layout/home-notes/index.tsx
@@ -0,0 +1,11 @@
+
+export function HomeNotes() {
+    return (
+        <div className="bg-white p-4 rounded-lg shadow-md m-4 h-20 text-center">
+            <div className="text-purple-600 font-semibold">Notes</div>
+            <p className="text-gray-600 text-sm">
+              Any saved retail orders will be deleted if they are not processed on the day the order was created.
+            </p>
+        </div>
+    )
+}
