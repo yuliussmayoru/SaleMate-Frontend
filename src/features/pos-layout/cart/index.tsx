7f28077fcaa6e199577a0d833c38452f28b68627
yuliussmayoru <yuliussmayoru@gmail.com>
2024-08-17 12:50:19 +0700

    fixing customer name position

--- src/features/pos-layout/cart/index.tsx
+++ src/features/pos-layout/cart/index.tsx
@@ -3,30 +3,31 @@ import { Button } from "@/src/features";
 export function Cart() {
     return (
         <div className="flex-1 bg-white p-4 rounded-lg shadow-md flex flex-col justify-between h-screen">
-          <div className="flex text-xl font-semibold">
-            <Button className="bg-gray-200 text-gray-500 w-full py-3 mt-4 rounded-lg">
-                Delete
-            </Button>
-            <div className="w-full text-center">
-                <p>Order Id</p>
-                <p>000000</p>
-            </div> 
-            <Button className="bg-gray-200 text-gray-500 w-full py-3 mt-4 rounded-lg">
-                Save
-            </Button>
-          </div>
-
-          <div>
-                <Button className="bg-gray-200 text-gray-500 w-full rounded-lg">
-                    <p>Customer Name</p>
-                </Button>
-          </div>
+            <div>
+                <div className="flex text-xl font-semibold">
+                    <Button className="bg-gray-200 text-gray-500 w-full py-3 mt-4 rounded-lg">
+                        Delete
+                    </Button>
+                    <div className="w-full text-center">
+                        <p>Order Id</p>
+                        <p>000000</p>
+                    </div> 
+                    <Button className="bg-gray-200 text-gray-500 w-full py-3 mt-4 rounded-lg">
+                        Save
+                    </Button>
+                </div>
+                <div>
+                    <Button className="bg-gray-200 text-gray-500 w-full rounded-lg mt-4">
+                        <p>Customer Name</p>
+                    </Button>
+                </div>
+            </div>
 
           <div className="text-center text-gray-400">
             Add new order or Continue from saved order
           </div>
           <div>
-            <div className="text-right text-gray-500 space-y-1">
+            <div className="text-left text-gray-500 space-y-1">
               <div>Subtotal (0 Items) -</div>
               <div>Tax -</div>
               <div className="font-bold">Grand Total -</div>
